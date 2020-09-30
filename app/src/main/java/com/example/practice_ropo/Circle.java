e7db1857cd2f1e3cc17227860e914e42450d89ab
lovettxh <60330069+lovettxh@users.noreply.github.com>
2020-09-30 14:38:25 -0600

    add something

--- app/src/main/java/com/example/practice_ropo/Circle.java
+++ app/src/main/java/com/example/practice_ropo/Circle.java
@@ -2,8 +2,11 @@ package com.example.practice_ropo;
 
 public class Circle {
     private double radius;
-
+    private String color;
     public double getArea(){
         return this.radius * Math.PI;
+    }
+    public String getColor(){
+        return this.color;
     }
 }
