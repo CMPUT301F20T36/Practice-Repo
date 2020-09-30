ed497a556ddb1ade683af462ffca1115ec58836d
Bailiang Liu <liu195265006@gmail.com>
2020-09-30 13:53:50 -0600

    Color field in Shape

--- app/src/main/java/com/example/practice_ropo/Shape.java
+++ app/src/main/java/com/example/practice_ropo/Shape.java
@@ -3,4 +3,5 @@ package com.example.practice_ropo;
 public abstract class Shape {
     private int x;
     private int y;
+    private String color = "blue";
 }
--- app/src/main/java/com/example/practice_ropo/Square.java
+++ app/src/main/java/com/example/practice_ropo/Square.java
@@ -1,4 +1,5 @@
 package com.example.practice_ropo;
 
-public class Square {
+public class Square extends Shape {
+
 }
