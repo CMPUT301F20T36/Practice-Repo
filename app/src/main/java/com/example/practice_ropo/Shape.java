15986b7e5c9ca2026fd1beed1f9652fdb303d3dd
Bailiang Liu <liu195265006@gmail.com>
2020-09-30 15:01:00 -0600

    Trying...

--- app/src/main/java/com/example/practice_ropo/Shape.java
+++ app/src/main/java/com/example/practice_ropo/Shape.java
@@ -3,13 +3,5 @@ package com.example.practice_ropo;
 public abstract class Shape {
     private int x;
     private int y;
-<<<<<<< HEAD
-<<<<<<< HEAD
-    private String color = "blue";
-=======
-    private String color = "$PUT_YOUR_CHOICE_OF_COLOR";
->>>>>>> 70b7680ccd8019c5cda0b5e9c9e7f7b493e9bf1b
-=======
-    private String color = "$PUT_YOUR_CHOICE_OF_COLOR";
->>>>>>> cae3e89a47b927574254a07f222f55bac63b658d
+    private String self_color = "blue";
 }
