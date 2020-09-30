a82da1dc27e159b85588fc40e24581fd8e63b187
Ang Li <lyon233333@.gmail.com>
2020-09-30 13:46:41 -0600

    add Starr class

--- app/src/main/java/com/example/practice_ropo/Shape.java
+++ app/src/main/java/com/example/practice_ropo/Shape.java
@@ -1,11 +1,6 @@
 package com.example.practice_ropo;
 
 public abstract class Shape {
-<<<<<<< HEAD
-    int x;
-    int y;
-=======
     private int x;
     private int y;
->>>>>>> origin/master
 }
---
+++ app/src/main/java/com/example/practice_ropo/Star.java
@@ -0,0 +1,4 @@
+package com.example.practice_ropo;
+
+public class Star extends Shape {
+}
