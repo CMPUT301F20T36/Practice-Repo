c93b2dfee6f39495fee7729ed9cebdae774f647f
Maocheng Shi <mshi1@ualberta.ca>
2020-10-01 00:04:21 -0600

    add my color

--- app/src/main/java/com/example/practice_ropo/Shape.java
+++ app/src/main/java/com/example/practice_ropo/Shape.java
@@ -3,5 +3,6 @@ package com.example.practice_ropo;
 public abstract class Shape {
     private int x;
     private int y;
+    private String my_color = "blue";
     private String color = "$PUT_YOUR_CHOICE_OF_COLOR";
 }
